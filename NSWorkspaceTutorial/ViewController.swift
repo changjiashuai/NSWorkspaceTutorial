9e5f98f3c890e952ddb4c3b25f34cc448c9bd2f9
changjiashuai <changjiashuai@gmail.com>
2015-11-17 23:38:14 +0800

    Initial Commit

---
+++ NSWorkspaceTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSWorkspaceTutorial
+//
+//  Created by CJS on 15/11/17.
+//  Copyright © 2015年 CJS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
